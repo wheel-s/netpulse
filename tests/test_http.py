3c7068df7ea6962d46151d4832e9a7bd237fae9c
wheel-s <toshirou2002@gmail.com>
2025-12-03 01:50:53 +0100

    C: fixed http_test in net.ci

--- tests/test_http.py
+++ tests/test_http.py
@@ -2,6 +2,6 @@ from netpulse.core_http import perform_http_request
 
 
 def test_get_request():
-    result = perform_http_request("https://www.jumia.com.ng/", "GET")
+    result = perform_http_request("https://google.com", "GET")
     assert result["success"] is True
     assert result["status_code"] == 200
