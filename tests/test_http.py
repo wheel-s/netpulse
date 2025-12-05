eec92744d83a2f1907a286345ca678a45d62da59
wheel-s <toshirou2002@gmail.com>
2025-12-05 14:48:13 +0100

    updated pyproject.toml

--- tests/test_http.py
+++ tests/test_http.py
@@ -4,4 +4,3 @@ from netpulse.core_http import perform_http_request
 def test_get_request():
     result = perform_http_request("https://google.com", "GET")
     assert result["success"] is True
-    
